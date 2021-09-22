934c235095afbe4d5dbe57e920a973930c1ee109
Geovanny Gil <geovannygr2018@gmail.com>
2021-09-22 02:04:23 -0600

    Modificiación de base de datos por suscripciones

--- guatelibro.sql
+++ guatelibro.sql
@@ -11,7 +11,7 @@ phone int(8),
 direction varchar(75),
 photo varchar(75),
 institution varchar(50),
-state int,
+state boolean,
 user_member varchar(50),
 password varchar(50),
 id_rol int);
@@ -48,24 +48,28 @@ date_months int(11));
 
 create table suscriptions(
 id_suscription int primary key auto_increment,
-id_member int,
-id_membership int,
-state int);
+id_payments int,
+subscription_start dateTime,
+subscription_end dateTime,
+date_cancel dateTime,
+state boolean,
+created_at timestamp);
 
 create table payments(
 id_payments int primary key auto_increment,
 id_member int,
-id_suscription int,
-payment decimal(12,2));
+id_membership int,
+payment_type varchar(100),
+payment decimal(12,2),
+created_at timestamp);
 
 alter table members ADD foreign key (id_rol) references rol(id_rol);
 alter table library_user ADD foreign key (id_member) references members(id_member);
 alter table library_user ADD foreign key (id_product) references product(id_product);
 alter table product ADD foreign key (id_member) references members(id_member);
 alter table product ADD foreign key (id_category) references category(id_category);
-alter table suscriptions ADD foreign key (id_member) references members(id_member);
-alter table suscriptions ADD foreign key (id_membership) references membership(id_membership);
+alter table suscriptions ADD foreign key (id_payments) references payments(id_payments);
 alter table payments ADD foreign key (id_member) references members(id_member);
-alter table payments ADD foreign key (id_suscription) references members(id_suscription);
+alter table payments ADD foreign key (id_membership) references membership(id_membership);
 
 
