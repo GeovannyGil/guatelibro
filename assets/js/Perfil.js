82af0b7c0df4e7e9c5d15d78ce61090a776ae861
AngelTeret <angelteret@gmail.com>
2021-10-06 01:01:54 -0600

    editar perfil nuevo

---
+++ assets/js/Perfil.js
@@ -0,0 +1,21 @@
+function enviar_usuario(id){
+    var formData= new FormData(document.getElementById('form-perfil'));
+    formData.append('id_member',id);
+    $.ajax({
+        type: "POST",
+        url: "http://localhost/guatelibro/Modificar/membersU",
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false
+
+    }).done(function (response) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Atención',
+            text: 'Se actualizo correctamente, Debes cerrar sesión para ver los cambios',
+        }).then(function () {
+            window.location.href = 'http://localhost/guatelibro/ver/bibliotecadigital';
+        });
+    }); 
+}
